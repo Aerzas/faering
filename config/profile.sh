76b16664cbc64b52df4d9ed5bacd7760f4ba291c
Aerzas <nicolas.ricklin@protonmail.com>
2020-08-04 10:55:43 +0200

    Fix profile export

--- config/profile.sh
+++ config/profile.sh
@@ -6,6 +6,6 @@ export USER_ID;
 
 # Faering environment variables.
 set -a
-[ -f "${FAERING:-~/.faering}/.env.dist" ] && export "$(grep "FAERING_" "${FAERING:-~/.faering}/.env.dist")";
-[ -f "${FAERING:-~/.faering}/.env" ] && export "$(grep "FAERING_" "${FAERING:-~/.faering}/.env")";
+[ -f "${FAERING:-~/.faering}/.env.dist" ] && export $(grep "FAERING_" "${FAERING:-~/.faering}/.env.dist");
+[ -f "${FAERING:-~/.faering}/.env" ] && export $(grep "FAERING_" "${FAERING:-~/.faering}/.env");
 set +a
